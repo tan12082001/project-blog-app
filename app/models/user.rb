550e1f4844b0ba98b9e57c233d663dc5fdf2b7a6
Tanmayi Manku <tanmayitanum@gmail.com>
2023-10-28 01:19:07 +0530

    update the class model with custom method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-    has_many :post, foreign_key :author_id
+    has_many :post, foreign_key: 'author_id'
     has_many :comment
     has_many :like
+
+    def three_recent_posts
+        Post.order(created_at: :desc).limit(3)
+    end
 end
