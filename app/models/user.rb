08dfc40f48330a91c79567c9380270ae6405cfd3
Tanmayi Manku <tanmayitanum@gmail.com>
2023-10-28 03:30:15 +0530

    update associations' namings, methods accordingly

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  has_many :post, foreign_key: 'author_id'
-  has_many :comment
-  has_many :like
+  has_many :posts, foreign_key: 'author_id'
+  has_many :comments
+  has_many :likes
 
   def three_recent_posts
-    Post.where(author: self).order(created_at: :desc).limit(3)
+    posts.where(author: self).order(created_at: :desc).limit(3)
   end
 end
