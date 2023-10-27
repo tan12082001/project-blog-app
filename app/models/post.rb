1ee0ebb2b1fadfb3ed164760274015ce4517bce3
Tanmayi Manku <tanmayitanum@gmail.com>
2023-10-28 01:22:02 +0530

    update post class with counter, comments methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ApplicationRecord
     belongs_to :author, class_name: 'User'
     has_many :comment
     has_many :like
+    after_save :update_posts_counter
+
+    def update_posts_counter
+        author.increment!(:posts_counter)
+    end
+
+    def five_recent_comments
+        Comment.order(created_at: :desc).limit(5)
+    end
 end
