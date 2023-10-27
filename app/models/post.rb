4c7ff5af34ba23c228c8b9e1da49019f4bad6941
Tanmayi Manku <tanmayitanum@gmail.com>
2023-10-28 01:53:14 +0530

    update the methods with where method

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,6 @@ class Post < ApplicationRecord
   end
 
   def five_recent_comments
-    Comment.order(created_at: :desc).limit(5)
+    Comment.where(post_id: id).order(created_at: :desc).limit(5)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_many :like
 
   def three_recent_posts
-    Post.order(created_at: :desc).limit(3)
+    Post.where(author: self).order(created_at: :desc).limit(3)
   end
 end
