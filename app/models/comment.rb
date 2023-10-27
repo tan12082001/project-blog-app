10068cdf41b9c5d8b846998810868e73fb184a5d
Tanmayi Manku <tanmayitanum@gmail.com>
2023-10-28 01:20:19 +0530

    update with methods counter, return 5 comments.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
-class Commnet < ApplicationRecord
+class Comment < ApplicationRecord
     belongs_to :post
     belongs_to :user
+
+    after_save :update_comments_counter
+
+    def update_comments_counter
+        post.increment!(:comments_counter)
+    end
 end
