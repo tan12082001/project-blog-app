26b6ce7ee258cf4565d0351ffbaf7c0109ffa244
Tanmayi Manku <tanmayitanum@gmail.com>
2023-10-31 09:52:48 +0530

    resources to consider index,show methods only

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root "users#index"
 
-  resources :users do
-    resources :posts
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
   end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
