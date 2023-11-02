953a3e0edce3be1a0386a1363d9e0ded64baeb43
Tanmayi Manku <tanmayitanum@gmail.com>
2023-11-02 17:07:05 +0530

    fix rubocop linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
-    layout 'boilerplate'
-    def index
+  layout 'boilerplate'
+  def index
     @user = User.find(params[:user_id])
     @posts = Post.where(author_id: params[:user_id]).order(id: :asc)
     @posts = @posts.paginate(page: params[:page], per_page: 2)
