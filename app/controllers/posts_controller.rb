dc790f5842fbf826de8c46022779742d8e95902c
Tanmayi Manku <tanmayitanum@gmail.com>
2023-11-10 09:51:53 +0530

    Add destroy action for Post Model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,6 +27,14 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.author.decrement!(:posts_counter)
+    @post.destroy!
+    flash[:success] = 'Post Deleted!'
+    redirect_to user_posts_url
+  end
+
   private
 
   def post_params
